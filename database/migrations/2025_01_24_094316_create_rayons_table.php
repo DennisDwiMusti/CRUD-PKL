5a361883f2abd5a470b97529222457836a6fce47
DennisDwiMusti <dennisdwi18@gmail.com>
2025-01-24 17:16:48 +0700

    Fix: migration tabel rayon

--- database/migrations/2025_01_24_094316_create_rayons_table.php
+++ database/migrations/2025_01_24_094316_create_rayons_table.php
@@ -1,9 +1,7 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
 class CreateRayonsTable extends Migration
 {
     /**
@@ -16,21 +14,18 @@ class CreateRayonsTable extends Migration
             $table->string('name')->unique();
             $table->timestamps();
         });
-
-        Schema::table('siswas', function (Blueprint $table) {
-            $table->unsignedBigInteger('rayon_id')->nullable()->after('rombel');
-            $table->foreign('rayon_id')->references('id')->on('rayons')->onDelete('set null');
+        Schema::table('siswa', function (Blueprint $table) {
+            $table->string('rayon')->nullable()->change();
         });
-    }
 
+    }
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::table('siswas', function (Blueprint $table) {
-            $table->dropForeign(['rayon_id']);
-            $table->dropColumn('rayon_id');
+        Schema::table('siswa', function (Blueprint $table) {
+            $table->string('rayon')->nullable(false)->change();
         });
 
         Schema::dropIfExists('rayons');
