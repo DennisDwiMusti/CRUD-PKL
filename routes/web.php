3f11f6dd1437c400494f719f8c1daa5af9de28ab
DennisDwiMusti <dennisdwi18@gmail.com>
2025-02-05 15:48:37 +0700

    fix: menambahkan route untuk data rayon

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\RayonController;
 
 Route::get('/', function () {
     return view('home');
 });
+Route::prefix('rayon')->name('rayon.')->group(function () {
+    Route::get('/', [RayonController::class, 'index'])->name('index');
+    Route::get('/tambah', [RayonController::class, 'create'])->name('create');
+    Route::post('/store', [RayonController::class, 'store'])->name('store');
+    Route::get('/{id}', [RayonController::class, 'edit'])->name('edit');
+    Route::patch('/{id}', [RayonController::class, 'update'])->name('update');
+    Route::delete('/{id}', [RayonController::class, 'destroy'])->name('destroy');
+});
+
 Route::prefix('siswa')->name('siswa.')->group(function () {
     Route::get('/', [SiswaController::class, 'index'])->name('index');
-    Route::get('/buat', [SiswaController::class, 'create'])->name('create');
+    Route::get('/tambah', [SiswaController::class, 'create'])->name('create');
     Route::post('/store', [SiswaController::class, 'store'])->name('store');
     Route::get('/{id}', [SiswaController::class, 'edit'])->name('edit');
     Route::patch('/{id}', [SiswaController::class, 'update'])->name('update');
